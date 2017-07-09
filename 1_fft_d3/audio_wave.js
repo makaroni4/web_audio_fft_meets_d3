5817757a57ad89dc781208526a5f5fe9c5e4f0a6
Anatoli Makarevich <makaroni4@gmail.com>
2017-07-09 16:41:56 +0200

    Initial commit

---
+++ 1_fft_d3/audio_wave.js
@@ -0,0 +1,51 @@
+function AudioWaveChart(className) {
+  var $audioWave = $("." + className);
+
+  var w = $audioWave.width();
+  var h = $audioWave.height();
+
+  var x_scale = d3.scaleLinear().range([0, w]).domain([0, 1]);
+  var y_scale = d3.scaleLinear().range([h, 0]).domain([0, 1]);
+
+  var line = d3.line()
+          .x(function(d) {
+              return x_scale(d.x);})
+          .y(function(d) {
+              return y_scale(d.y);
+          })
+
+  var graph = d3.select("." + className).append("svg:svg")
+                .attr("width", w)
+                .attr("height", h)
+                .append("svg:g");
+
+  graph.append("svg:path").attr("class", className + "-line");
+
+  var setDomain = function(data_xy){
+    x_scale.domain(d3.extent(data_xy, function(d){ return d.x}));
+
+    var y_range = d3.extent(data_xy, function(d){ return d.y});
+
+    y_scale.domain([ Math.min(-0.1, y_range[0]), Math.max(0.1, y_range[1]) ]);
+  };
+
+  var plotD3Wave = function(data_xy) {
+    setDomain(data_xy);
+    var svg = d3.select("body").transition();
+    svg.select("." + className + "-line")
+       .duration(0)
+       .attr("d", line(data_xy));
+  }
+
+  return {
+    plotWave: function(wave) {
+      var data_xy  = [];
+
+      for(var i = 0; i < wave.length; i++){
+        data_xy.push({x: i, y: wave[i]});
+      }
+
+      plotD3Wave(data_xy);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Web Audio API meets d3
+
+## Description
+
+A bunch of visualisations for [Guitar Bro](https://makaroni4.github.io/guitar_bro/).
+
+Namely:
+
+1. Raw visualisation of [Time Buffer](https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getFloatTimeDomainData) & [Wave buffers](https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getFloatFrequencyData).
